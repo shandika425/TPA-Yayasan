a61e8c0fa0c05b19c8f7cc6fc7a9ecedd507fb1b
Fatimah Azzahra <165116410+11Fatimah@users.noreply.github.com>
2025-05-17 18:21:30 +0700

    Delete views directory

--- views/admin/form.php
+++
@@ -1,6 +0,0 @@
-<h3>Form Admin</h3>
-<form method="post">
-    <!-- Tambahkan input sesuai kebutuhan -->
-    <input type="text" name="nama" placeholder="Nama" value="<?= isset($row) ? $row->nama : '' ?>">
-    <button type="submit">Simpan</button>
-</form>
